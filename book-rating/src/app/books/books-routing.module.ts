e4a29080e52f998ff0a4ef30f0288eeac9c0bac1
Ferdinand Malcher <ferdinand@malcher.media>
2020-10-19 13:13:56 +0200

    Routing

--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/search', component: SearchComponent },
+  { path: 'books/create', component: CreateBookComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './search/search.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    BookDetailsComponent,
+    SearchComponent,
+    CreateBookComponent
   ],
   imports: [
     CommonModule,
